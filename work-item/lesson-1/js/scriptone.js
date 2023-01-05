5c55d79dee0858d1bc563952e0bff4d3976596e2
Vladimir <cremneffv@gmail.com>
2023-01-05 18:21:27 +0300

    add new folder

---
+++ work-item/lesson-1/js/scriptone.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+//                           УРОК 6 - ОБЪЕКТЫ И МАССИВЫ! 
+
+//                                  ОБЪЕКТЫ:
+
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+const arrOneObj = {
+    0: 'a',
+    1: 'b',
+    2: 'c'
+};
+
+// Вот так вызываются свойства из объекта!
+console.log(obj.name);
+console.log(obj['age']);
+
+//                                   МАССИВЫ:
+
+
+//Нумерация по умолчанию по индексу: 0, 1, 2, 3
+const arrOne = ['a', 'b', 'c'];
+
+arrOne[a] = 0;
+arrOne[b] = 1;
+arrOne[c] = 2;
+
+
+let arrTwo = ['plum.png', 'orange.jpg', 6, {}, [] ];
+console.log(arr[1]);
+
+//Синтаксические возможности
+
+//                           УРОК 7 - Простое общение с пользователем!
+//                                    document.write- и -typeOf
