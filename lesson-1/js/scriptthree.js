432d5e89ad8bb48adc83882ea1feb1601c17e2d1
Vladimir <cremneffv@gmail.com>
2023-01-05 17:31:03 +0300

    first commit

---
+++ lesson-1/js/scriptthree.js
@@ -0,0 +1,11 @@
+//                                                    Урок 8.    ИНТЕРПОЛЯЦИЯ.
+
+const category = 'toys';
+
+// console.log('https;//someurl.com/' + category + '/' + '4'); // конкатенация (склеивание) строк!
+console.log(`https;//someurl.com/${category}/5`); // Это и есть интерполяция!
+
+
+const user = 'Ivan';
+
+alert(`Привет. ${user}`); // Еще пример!
